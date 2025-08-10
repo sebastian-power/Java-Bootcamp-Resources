ac0f0709221dc8e50849dca63d781e0e82fc6c20
Sebastian Power <sebastianrosspower@gmail.com>
2025-08-10 11:57:19 +1000

    First Workbook done

--- Module 1 - Introduction to Java/1. Getting Started/Workbook 1.1/FirstProgram.java
+++ Module 1 - Introduction to Java/1. Getting Started/Workbook 1.1/FirstProgram.java
@@ -1,2 +1,7 @@
 // I'm a comment. I have no impact on your code.
 // Instructions for this workbook are on Learn the Part (See the Udemy Video: Workbook 1.1 to access the link).
+public class FirstProgram {
+    public static void main (String[] args) {
+        System.out.println("No");
+    }
+}
